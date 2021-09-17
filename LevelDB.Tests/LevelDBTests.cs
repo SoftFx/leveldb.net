2279783a1b40a95bfd8cda5aab0b717f8d2c1c53
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2021-09-17 11:24:51 +0300

    Tests refactoring

--- LevelDB.Tests/LevelDBTests.cs
+++ LevelDB.Tests/LevelDBTests.cs
@@ -10,177 +10,205 @@ namespace LevelDB.NET.Tests
     [TestFixture]
     public class LevelDBTests
     {
-        static string testPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Test");
+        DB Database { get; set; }
+        string DatabasePath { get; set; }
 
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
+        [SetUp]
+        public void SetUp()
         {
-            Directory.Delete(testPath, true);
+            var tempPath = Path.GetTempPath();
+            var randName = Path.GetRandomFileName();
+            DatabasePath = Path.Combine(tempPath, randName);
+            var options = new Options()
+            {
+                CreateIfMissing = true
+            };
+            Database = new DB(DatabasePath, options);
         }
 
-        static string CleanTestDB()
+        [TearDown]
+        public void TearDown()
         {
-            DB.Destroy(testPath, new Options { CreateIfMissing = true });
-            return testPath;
+            // some test-cases tear-down them self
+            if (Database != null)
+            {
+                Database.Dispose();
+            }
+            if (Directory.Exists(DatabasePath))
+            {
+                Directory.Delete(DatabasePath, true);
+            }
+        }
+
+
+        [Test]
+        public void Close()
+        {
+            // test double close
+            Database.Dispose();
+        }
+
+        [Test]
+        public void DisposeChecks()
+        {
+            Database.Dispose();
+            Assert.That(() => Database.Get("key1"), Throws.TypeOf<ObjectDisposedException>());
+        }
+
+        [Test]
+        public void Error()
+        {
+            var options = new Options()
+            {
+                CreateIfMissing = false
+            };
+            Assert.That(() => new DB("non-existent", options), Throws.TypeOf<LevelDBException>());
         }
 
         [Test]
         public void TestCRUD()
         {
-            var path = CleanTestDB();
+            Database.Put("Tampa", "green");
+            Database.Put("London", "red");
+            Database.Put("New York", "blue");
 
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
-            {
-                db.Put("Tampa", "green");
-                db.Put("London", "red");
-                db.Put("New York", "blue");
+            Assert.AreEqual(Database.Get("Tampa"), "green");
+            Assert.AreEqual(Database.Get("London"), "red");
+            Assert.AreEqual(Database.Get("New York"), "blue");
 
-                Assert.AreEqual(db.Get("Tampa"), "green");
-                Assert.AreEqual(db.Get("London"), "red");
-                Assert.AreEqual(db.Get("New York"), "blue");
+            Database.Delete("New York");
 
-                db.Delete("New York");
+            Assert.IsNull(Database.Get("New York"));
 
-                Assert.IsNull(db.Get("New York"));
-
-                db.Delete("New York");
-            }
+            Database.Delete("New York");
         }
 
         [Test]
         public void TestRepair()
         {
             TestCRUD();
-            DB.Repair(testPath, new Options());
+            DB.Repair(DatabasePath, new Options());
         }
 
         [Test]
         public void TestIterator()
         {
-            var path = CleanTestDB();
+            Database.Put("Tampa", "green");
+            Database.Put("London", "red");
+            Database.Put("New York", "blue");
 
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
+            var expected = new[] { "London", "New York", "Tampa" };
+            
+            var actual = new List<string>();
+            using (var iterator = Database.CreateIterator(new ReadOptions()))
             {
-                db.Put("Tampa", "green");
-                db.Put("London", "red");
-                db.Put("New York", "blue");
-
-                var expected = new[] { "London", "New York", "Tampa" };
-
-                var actual = new List<string>();
-                using (var iterator = db.CreateIterator(new ReadOptions()))
+                iterator.SeekToFirst();
+                while (iterator.Valid())
                 {
-                    iterator.SeekToFirst();
-                    while (iterator.Valid())
-                    {
-                        var key = iterator.StringKey();
-                        actual.Add(key);
-                        iterator.Next();
-                    }
+                    var key = iterator.StringKey();
+                    actual.Add(key);
+                    iterator.Next();
                 }
-
-                CollectionAssert.AreEqual(expected, actual);
             }
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
         public void TestEnumerable()
         {
-            var path = CleanTestDB();
+            Database.Put("Tampa", "green");
+            Database.Put("London", "red");
+            Database.Put("New York", "blue");
 
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
-            {
-                db.Put("Tampa", "green");
-                db.Put("London", "red");
-                db.Put("New York", "blue");
+            var expected = new[] { "London", "New York", "Tampa" };
+            var actual = from kv in Database as IEnumerable<KeyValuePair<string, string>>
+                           select kv.Key;
 
-                var expected = new[] { "London", "New York", "Tampa" };
-                var actual = from kv in db as IEnumerable<KeyValuePair<string, string>>
-                             select kv.Key;
-
-                CollectionAssert.AreEqual(expected, actual.ToArray());
-            }
+            CollectionAssert.AreEqual(expected, actual.ToArray());
         }
 
         [Test]
         public void TestSnapshot()
         {
-            var path = CleanTestDB();
+            Database.Put("Tampa", "green");
+            Database.Put("London", "red");
+            Database.Delete("New York");
 
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
+            using (var snapShot = Database.CreateSnapshot())
             {
-                db.Put("Tampa", "green");
-                db.Put("London", "red");
-                db.Delete("New York");
+                var readOptions = new ReadOptions { Snapshot = snapShot };
 
-                using (var snapShot = db.CreateSnapshot())
-                {
-                    var readOptions = new ReadOptions { Snapshot = snapShot };
+            Database.Put("New York", "blue");
 
-                    db.Put("New York", "blue");
+                Assert.AreEqual(Database.Get("Tampa", readOptions), "green");
+                Assert.AreEqual(Database.Get("London", readOptions), "red");
 
-                    Assert.AreEqual(db.Get("Tampa", readOptions), "green");
-                    Assert.AreEqual(db.Get("London", readOptions), "red");
-
-                    // Snapshot taken before key was updates
-                    Assert.IsNull(db.Get("New York", readOptions));
-                }
-
-                // can see the change now
-                Assert.AreEqual(db.Get("New York"), "blue");
+                // Snapshot taken before key was updates
+                Assert.IsNull(Database.Get("New York", readOptions));
             }
+
+            // can see the change now
+            Assert.AreEqual(Database.Get("New York"), "blue");
         }
 
         [Test]
         public void TestGetProperty()
         {
-            var path = CleanTestDB();
-
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
+            var r = new Random(0);
+            var data = "";
+            for (var i = 0; i < 1024; i++)
             {
-                var r = new Random(0);
-                var data = "";
-                for (var i = 0; i < 1024; i++)
-                {
-                    data += 'a' + r.Next(26);
-                }
-
-                for (int i = 0; i < 5 * 1024; i++)
-                {
-                    db.Put(string.Format("row{0}", i), data);
-                }
-
-                var stats = db.PropertyValue("leveldb.stats");
-
-                Assert.IsNotNull(stats);
-                Assert.IsTrue(stats.Contains("Compactions"));
+                data += 'a' + r.Next(26);
             }
+
+            for (int i = 0; i < 5 * 1024; i++)
+            {
+                Database.Put(string.Format("row{0}", i), data);
+            }
+
+            var stats = Database.PropertyValue("leveldb.stats");
+
+            Console.WriteLine(stats);
+
+            Assert.IsNotNull(stats);
+            Assert.IsTrue(stats.Contains("Compactions"));
         }
 
         [Test]
         public void TestWriteBatch()
         {
-            var path = CleanTestDB();
+            Database.Put("NA", "Na");
 
-            using (var db = new DB(path, new Options { CreateIfMissing = true }))
+            using (var batch = new WriteBatch())
             {
-                db.Put("NA", "Na");
-
-                using (var batch = new WriteBatch())
-                {
-                    batch.Delete("NA")
-                         .Put("Tampa", "Green")
-                         .Put("London", "red")
-                         .Put("New York", "blue");
-                    db.Write(batch);
-                }
-
-                var expected = new[] { "London", "New York", "Tampa" };
-                var actual = from kv in db as IEnumerable<KeyValuePair<string, string>>
-                             select kv.Key;
-
-                CollectionAssert.AreEqual(expected, actual.ToArray());
+                batch.Delete("NA")
+                     .Put("Tampa", "Green")
+                     .Put("London", "red")
+                     .Put("New York", "blue");
+                Database.Write(batch);
             }
+
+            var expected = new[] { "London", "New York", "Tampa" };
+            var actual = from kv in Database as IEnumerable<KeyValuePair<string, string>>
+                         select kv.Key;
+
+            CollectionAssert.AreEqual(expected, actual.ToArray());
         }
+
+        [Test]
+        public void IsValid()
+        {
+            Database.Put("key1", "value1");
+
+            var iter = Database.CreateIterator();
+            iter.SeekToLast();
+            Assert.IsTrue(iter.IsValid);
+
+            iter.Next();
+            Assert.IsFalse(iter.IsValid);
+        }
+
     }
+
 }
